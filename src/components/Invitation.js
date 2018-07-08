5aab355d1c8dc431885c858b63babc64f7db2dea
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-08 14:11:25 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,20 @@
 // Code Invitation Component Here
+// In the components/Invitation.js file, create an Invitation React component.
+// This component renders out an h1 with the text 'You've been invited!'
+// After this h1, render out any children that are passed into the Invitation component. This allows us to customize the message.
+//
+import React from "react" 
+
+class Invitation extends React.Component {
+  render() {
+    return (
+      <div> 
+        <h1>You've been invited!</h1>
+        <h2>{this.props.children}</h2>
+
+      </div>
+    )
+  }
+}
+
+export default Invitation 
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,20 @@
 // Code ThemedDecoration Component Here
+import React from "react" 
+
+class ThemedDecoration extends React.Component {
+  render() {
+
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {className: this.props.theme, another: "thing"})
+      // Children automatically handles mapping with new keys assigned
+    });
+
+    return (
+      <div> 
+        {childrenWithExtraProp}
+      </div>
+    )
+  }
+}
+
+export default ThemedDecoration 
